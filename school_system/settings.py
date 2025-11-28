2e16c2df28b1b806ff8b41982680a3b3433e8735
dennielestira <estiradenniel.cvsu@gmail.com>
2025-11-28 13:28:28 +0800

    database

--- school_system/settings.py
+++ school_system/settings.py
@@ -99,13 +99,22 @@ WSGI_APPLICATION = 'school_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Service',
+        'USER': 'postgres',
+        'PASSWORD': 'Extension',
+        'PORT' : '5432',
+        'HOST' : 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
